5ce6dbbd838ac0a16132d31480171d4f71807e9a
Willits-Alec <alecwillits@gmail.com>
2020-09-17 17:40:27 -0600

    added book code

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,18 +11,44 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string name, location;
+            string name = string.Empty;
+            string location = string.Empty;
 
-            name = "Alec Willits";
-            location = "Seattle, Washington";
+            Console.WriteLine("Please enter your full name");
+            name = Console.ReadLine();
 
-            Console.WriteLine("My name is " + name);
-            Console.WriteLine("I am from " + location);
+            Console.WriteLine("Where are you from?");
+            location = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {name} You are from {location}");
+
+            var currentDate = DateTime.Now;
+            Console.WriteLine(currentDate.ToShortDateString());
+
+            //Come from 2.1
+
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            Console.Write("Please Enter width in meters: ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            Console.Write("Enter Height: ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            //processing
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+
+
+            Console.WriteLine("The length of the wood is " + woodLength + " feet");
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+
 
             Console.WriteLine("Press any key to exit");
-
             Console.ReadKey();
-
         }
     }
 }

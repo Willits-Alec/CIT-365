0d346f542afa89d4913d6d95a8f725e12be3022f
Willits-Alec <alecwillits@gmail.com>
2020-09-17 13:45:09 -0600

    added output

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, location;
+
+            name = "Alec Willits";
+            location = "Seattle, Washington";
+
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("I am from " + location);
+
+            Console.WriteLine("Press any key to exit");
+
+            Console.ReadKey();
+
+        }
+    }
+}
